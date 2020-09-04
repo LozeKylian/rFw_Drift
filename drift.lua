e8ea7b18de0ace4edaa430e16aa6a0876c337ad1
LozeKylian <66750292+LozeKylian@users.noreply.github.com>
2020-09-04 12:23:09 +0200

    Add files via upload

---
+++ drift.lua
@@ -0,0 +1,39 @@
+local drift = false
+local kmh = 3.6
+local drift_speed_limit = 130.00
+CreateThread(function()
+    while true do
+        Wait(0)
+        if IsPedInAnyVehicle(GetPed(), false) then
+
+             local CarSpeed = GetEntitySpeed(GetCar()) * kmh
+
+            if GetPedInVehicleSeat(GetCar(), -1) == GetPed() then
+
+                if CarSpeed <= 130.00 then  
+                    if drift then
+                        SetVehicleReduceGrip(GetCar(), true)
+                    else
+                        SetVehicleReduceGrip(GetCar(), false)
+                    end
+                end
+            end
+        end
+    end
+end)
+RegisterCommand('+drift', function()
+    --Presse I donc le drift s'active
+    drift = true
+    if IsPedInAnyVehicle(GetPed(), false) then
+        ShowNotification("Le ~g~drift ~w~est disponible ~o~uniquement ~r~en dessous de 130 km/h.")
+    end
+end, false)
+RegisterCommand('-drift', function()
+    --Relachement de la touche drift donc le drift est off
+    drift = false
+end, false)
+RegisterKeyMapping('+drift', 'Drift', 'keyboard', 'i')
+
+-- Function
+function GetPed() return GetPlayerPed(-1) end
+function GetCar() return GetVehiclePedIsIn(GetPlayerPed(-1),false) end
